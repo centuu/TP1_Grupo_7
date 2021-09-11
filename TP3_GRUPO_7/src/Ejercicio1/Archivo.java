af96cb9417cc94d90195284bc290d8dee92900fb
Agustin Carusillo <acarusillo@request.com.ar>
2021-09-11 13:06:12 -0300

    Se crearon todas las funciones necesarias

--- TP3_GRUPO_7/src/Ejercicio1/Archivo.java
+++ TP3_GRUPO_7/src/Ejercicio1/Archivo.java
@@ -3,8 +3,52 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.File;
 
-public class Archivo {	
+public class Archivo {
+	
+	String ruta;
+	
+	public String getRuta() {
+		return ruta;
+	}
+
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+	
+	public boolean crearArchivo(String path)
+	{
+		FileWriter escritura;
+		try
+		{
+			escritura = new FileWriter (path,true);
+			escritura.write("");
+			escritura.close();
+			return true;
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean existe (String path)
+	{
+		File archivo = new File (path);
+		if(archivo.exists())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}	
+
 	public static ArrayList<String> leer_Archivo(String path)
 	{		
 		ArrayList<String> registros = new ArrayList<String>();
@@ -33,4 +77,44 @@ public class Archivo {
 		
 		return registros;
      }	
+	
+	public void escribeLineas (String linea)
+	{
+		try
+		{
+			FileWriter entrada = new FileWriter (ruta,true);
+			BufferedWriter miBuffer = new BufferedWriter(entrada);
+			miBuffer.write(linea);
+			miBuffer.close();
+			entrada.close();		
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void leeLineas()
+	{
+		FileReader entrada;
+		try
+		{
+			entrada = new FileReader(ruta);
+			BufferedReader miBuffer = new BufferedReader(entrada);
+			
+			String linea = "";
+			while(linea != null)
+			{
+				System.out.println(linea);
+				linea = miBuffer.readLine();
+			}
+			
+			miBuffer.close();
+			entrada.close();
+		}
+		catch (IOException e)
+		{
+			System.out.println("No se encontro el archivo");
+		}
+	}
 }
--- TP3_GRUPO_7/src/Ejercicio1/Main.java
+++ TP3_GRUPO_7/src/Ejercicio1/Main.java
@@ -4,7 +4,25 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		Persona.leerPersonasDesdeArchivo();
-		Persona.mostrarPersonas();
+		/*Persona.leerPersonasDesdeArchivo();
+		Persona.mostrarPersonas();*/
+		
+		Archivo archivo = new Archivo();
+		archivo.setRuta("Prueba.txt");
+		
+		if(archivo.existe(archivo.getRuta()))
+		{
+			System.out.println("Existe Archivo");
+		}
+		else
+		{
+			System.out.println("No existe el archivo, por lo tanto sera creado");
+			archivo.crearArchivo(archivo.getRuta());
+		}		
+		
+		archivo.escribeLineas("Hola todo bien?");
+		
+		System.out.println("\nA continuacion se lee la linea");
+		archivo.leeLineas();
 	}
 }
