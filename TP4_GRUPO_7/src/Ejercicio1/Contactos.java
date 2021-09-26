b30e12aa90bbda79b6681b88e5f6db1875df1d98
Pablo Torres <p.torres0991@gmail.com>
2021-09-26 01:13:28 -0300

    Validaciones en clase Contacto del Ejercicio1.

--- TP4_GRUPO_7/src/Ejercicio1/Contactos.java
+++ TP4_GRUPO_7/src/Ejercicio1/Contactos.java
@@ -1,5 +1,6 @@
 package Ejercicio1;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,15 +15,17 @@ import javax.swing.border.EmptyBorder;
 public class Contactos extends JFrame  
 {
 	private static final long serialVersionUID = 1L;
-	private static JTextField textField = new JTextField();
-	private static JTextField textField_1 = new JTextField();
-	private static JTextField textField_2 = new JTextField();
-	private static JTextField textField_3 = new JTextField();
+	private JPanel contentPane;
+	private static JTextField txtNombre = new JTextField();
+	private static JTextField txtApellido = new JTextField();
+	private static JTextField txtTelefono = new JTextField();
+	private static JTextField txtFechaNacimiento = new JTextField();
+	private static JLabel lblResultado = new JLabel();
 	private static JButton btnMostrar = new JButton("Mostrar");
 	
 	public Contactos()
 	{
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		//setBounds(100, 100, 450, 300);
 	    setSize(402,400);
 		setLocation(350, 150);
@@ -44,26 +47,30 @@ public class Contactos extends JFrame
 		lbTelefono.setBounds(85, 112, 46, 14);
 		this.getContentPane().add(lbTelefono);
 		
-		textField.setBounds(161, 48, 86, 20);
-		this.getContentPane().add(textField);
-		textField.setColumns(10);
+		JLabel lblResultado = new JLabel("Resultado: ");
+		lblResultado.setBounds(21, 298, 333, 14);
+		this.getContentPane().add(lblResultado);
 		
-		textField_1.setColumns(10);
-		textField_1.setBounds(161, 73, 86, 20);
-		this.getContentPane().add(textField_1);
+		txtNombre.setBounds(161, 48, 86, 20);
+		this.getContentPane().add(txtNombre);
+		txtNombre.setColumns(10);
 		
-		textField_2.setColumns(10);
-		textField_2.setBounds(161, 109, 86, 20);
-		this.getContentPane().add(textField_2);
+		txtApellido.setColumns(10);
+		txtApellido.setBounds(161, 73, 86, 20);
+		this.getContentPane().add(txtApellido);
 		
-		textField_3.setColumns(10);
-		textField_3.setBounds(161, 145, 86, 20);
-		this.getContentPane().add(textField_3);
+		txtTelefono.setColumns(10);
+		txtTelefono.setBounds(161, 109, 86, 20);
+		this.getContentPane().add(txtTelefono);
+		
+		txtFechaNacimiento.setColumns(10);
+		txtFechaNacimiento.setBounds(161, 145, 86, 20);
+		this.getContentPane().add(txtFechaNacimiento);
 		
 
 		btnMostrar.setBounds(141, 225, 89, 23);
 		this.getContentPane().add(btnMostrar);
-		btnMostrar.addActionListener(new EventoMostar());
+		btnMostrar.addActionListener(new EventoMostar(txtNombre, txtApellido, txtTelefono, txtFechaNacimiento, lblResultado));
 
 		((JPanel)this.getContentPane()).setBorder(new EmptyBorder(5, 5, 5, 5));
 		this.getContentPane().setLayout(null);
@@ -74,11 +81,62 @@ public class Contactos extends JFrame
 
 class EventoMostar implements ActionListener
 {
+	private JTextField txtNombre;
+	private JTextField txtApellido;
+	private JTextField txtTelefono;
+	private JTextField txtNacimiento;
+	private JLabel lblResultado;
+	
+	public EventoMostar(JTextField txtName, JTextField txtApe, JTextField txtTel, JTextField txtNac, JLabel lblResult)
+	{
+		txtNombre = txtName;
+		txtApellido = txtApe;
+		txtTelefono = txtTel;
+		txtNacimiento = txtNac;
+		lblResultado = lblResult;
+	}
+	
+	public boolean CheckIsEmpty(JTextField txtField)
+	{
+		if(txtField.getText().isEmpty())
+		{
+			txtField.setBackground(Color.red);
+			return true;
+		}
+		else
+		{
+			txtField.setBackground(Color.white);
+			return false;
+		}
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Mostrar");	
+		boolean isCompleted = false;
+		
+		if(!CheckIsEmpty(txtNombre))
+		{
+			isCompleted = true;
+		}
+		if(!CheckIsEmpty(txtApellido))
+		{
+			isCompleted = true;
+		}
+		if(!CheckIsEmpty(txtTelefono))
+		{
+			isCompleted = true;
+		}
+		if(!CheckIsEmpty(txtNacimiento))
+		{
+			isCompleted = true;
+		}
+		
+		if(isCompleted==true)
+		{
+			lblResultado.setText("Resultado: " +  txtNombre.getText() + "-" + txtApellido.getText() + "-" + txtTelefono.getText() + " " + txtNacimiento.getText());
+		}
 	}
 	
 	
