e3c348455796852e3fec9e34bbbfc0a39f1b4383
tomsct <64430192+gacelon@users.noreply.github.com>
2021-10-18 20:37:39 -0300

    Se agrego Listar Personas

--- TP6_GRUPO_7/src/presentacion/vista/PanelListar.java
+++ TP6_GRUPO_7/src/presentacion/vista/PanelListar.java
@@ -1,19 +1,26 @@
 package presentacion.vista;
 
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import daoImpl.PersonaDaoIml;
+import entidad.Persona;
+
 import javax.swing.UIManager;
 import javax.swing.JScrollPane;
 
 public class PanelListar extends JPanel
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JTable table;
-	public PanelListar() {
+	private DefaultTableModel modelPersonas;
+	private String[] nombreColumnas = {"Nombre", "Apellido","DNI"};
+	
+	public PanelListar() 
+	{
 		setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -23,25 +30,46 @@ public class PanelListar extends JPanel
 		table = new JTable();
 		scrollPane.setViewportView(table);
 		table.setBorder(UIManager.getBorder("ComboBox.editorBorder"));
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Nombre", "Apellido", "DNI"
-			}
-		) {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-			boolean[] columnEditables = new boolean[] {
-				false, false, false
-			};
-			public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
-		});
+		
+		modelPersonas = new DefaultTableModel(null,nombreColumnas);
+		table = new JTable(modelPersonas);
+
 		table.getColumnModel().getColumn(2).setResizable(false);
 		table.getColumnModel().getColumn(2).setPreferredWidth(74);
+		
+		scrollPane.setViewportView(table);
+		
+		this.LlenarTabla();
+	}
+	
+	public DefaultTableModel getModelPersonas() 
+	{
+		return modelPersonas;
+	}
+	
+	public String[] getNombreColumnas() 
+	{
+		return nombreColumnas;
+	}
+	
+	public void LlenarTabla()
+	{
+		PersonaDaoIml mn = new PersonaDaoIml();
+		
+		ArrayList<Persona> lista = mn.List();
+		
+		this.getModelPersonas().setRowCount(0); 
+		this.getModelPersonas().setColumnCount(0);
+		this.getModelPersonas().setColumnIdentifiers(this.getNombreColumnas());
+
+		for (Persona p : lista)
+		{
+			String nombre = p.getNombre();
+			String apellido = p.getApellido();
+			String DNI = p.getDni();
+			Object[] fila = {nombre, apellido, DNI};
+			
+			this.getModelPersonas().addRow(fila);
+		}
 	}
 }
